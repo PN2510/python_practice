5b184d8703f3900fca590a885d9a00f7cfbec99e
PN2510 <narpranay31@gmail.com>
2025-06-09 01:45:22 +0530

    .

--- prime_factors_num.py
+++ prime_factors_num.py
@@ -1,10 +1,11 @@
 def prime_factors(num):
-    factors=[]
+    factors = []
     factor = 2
+
     while(num>=2):
-        if(num%factor==0):
+        if (num%factor==0):
             factors.append(factor)
-            num = num/factor
+            num = num / factor
         else:
             factor+=1
     return factors
