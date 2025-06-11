beb039bd394e5003da5a56b64cc4c24e9e4a52db
PN2510 <narpranay31@gmail.com>
2025-06-12 00:37:52 +0530

    simple if else calculator

---
+++ if_else_calculator.py
@@ -0,0 +1,11 @@
+num1 = int(input('enter a number'))
+num2 = int(input('enter a number'))
+operator = input("enter a operator: ")
+if operator == "+":
+    print(num1+num2)
+if operator == "-":
+    print(num1-num2)
+if operator == "*":
+    print(num1*num2)
+if operator == "/":
+    print(num1/num2)
